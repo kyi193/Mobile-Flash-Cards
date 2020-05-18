4448e0dfb20e5c01cd5277aa95a046b072b49a7e
Kevin <kevin.yi93@gmail.com>
2020-05-18 13:41:20 -0400

    Create components folder with DeckList component and actions file

---
+++ actions/index.js
@@ -0,0 +1,20 @@
+export const ADD_DECK = 'ADD_DECK';
+export const ADD_CARD = 'ADD_CARD';
+export const RECEIVE_DECKS = 'RECEIVE_DECKS'
+
+
+export function addDeck(deck) {
+  return {
+    type: ADD_DECK,
+    deck,
+  }
+}
+
+export function addCard(card) {
+  return {
+    type: ADD_CARD,
+    card,
+  }
+}
+
+export function receiveDecks()
---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+class DeckList extends Component {
+  render() {
+    return (
+      <Text>
+        DeckList
+      </Text>
+    )
+  }
+}
+
+export default DeckList;
