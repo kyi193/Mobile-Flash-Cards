24746c84decf1f4e262f8b8d3756568fe0b79c27
Kevin <kevin.yi93@gmail.com>
2020-05-18 13:55:53 -0400

    Create initial deck data in api file

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+export const getInitialData = {
+  Poker: {
+    title: 'Poker',
+    questions: [
+      {
+        question: 'What is the best hand dealt in No Limit Texas Hold Em?',
+        answer: 'Pocket Aces'
+      },
+      {
+        question: 'Which position acts the last after post flop?',
+        answer: 'The button'
+      }
+    ]
+  }
+}
