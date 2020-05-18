28582feddd3b27ff971daf60c922f8d8a1ff2e84
Kevin <kevin.yi93@gmail.com>
2020-05-18 13:07:33 -0400

    Create actions, reducers, middleware, and utils folders

--- App.js
+++ App.js
@@ -5,12 +5,7 @@ import { StyleSheet, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <FontAwesome name="apple" size={25} />
-      <MaterialIcons name="star" color="blue" size={25} />
-      {/* Create a button */}
-      <FontAwesome.Button name="facebook" backgroundColor="#3b5998" onPress={() => {}}>
-        Login with Facebook
-      </FontAwesome.Button>
+
     </View>
   );
 }
@@ -18,8 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
