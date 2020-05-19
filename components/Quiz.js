0c7c4c41564db6a72ba17b8b70fdf50c5a2307eb
Kevin <kevin.yi93@gmail.com>
2020-05-19 17:56:40 -0400

    Display total questions answered correctly and percentage(Green means more than 75% right and red if less)

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { blue, lightGreen, white, tomatoRed } from '../utils/colors'
 import { connect } from 'react-redux'
+
+const questionPercentage = (numerator, denominator) => {
+  return ((numerator / denominator) * 100).toFixed(0)
+}
+
+function CorrectBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={Platform.OS === 'ios'
+        ? styles.iosCorrectBtn
+        : styles.androidCorrectBtn}
+      onPress={onPress}>
+      <Text style={styles.correctBtnText}>Correct</Text>
+    </TouchableOpacity>
+  )
+}
+function IncorrectBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={Platform.OS === 'ios'
+        ? styles.iosIncorrectBtn
+        : styles.androidIncorrectBtn}
+      onPress={onPress}>
+      <Text style={styles.incorrectBtnText}>Wrong</Text>
+    </TouchableOpacity>
+  )
+}
 class Quiz extends Component {
   constructor(props) {
     super(props)
@@ -17,28 +45,50 @@ class Quiz extends Component {
       seeAnswerToggle: !this.state.seeAnswerToggle
     })
   }
+
+  submitCorrect = () => {
+    this.setState({
+      questionsAnswered: this.state.questionsAnswered + 1,
+      correctAnswers: this.state.correctAnswers + 1,
+      seeAnswerToggle: false,
+    })
+  }
+
+  submitIncorrect = () => {
+    this.setState({
+      questionsAnswered: this.state.questionsAnswered + 1,
+      seeAnswerToggle: false,
+    })
+  }
   render() {
     const { questionList } = this.props
     const { questionsAnswered, correctAnswers, seeAnswerToggle } = this.state
     const deckLength = questionList.length
     return (
-      <View>
-        {questionList === deckLength
+      <View style={styles.container}>
+        {questionsAnswered === deckLength
           ? <View>
-            <Text>You got {correctAnswers} questions correct!</Text>
+            <Text style={styles.correctQuestions}>You got {correctAnswers} questions correct!</Text>
+            {questionPercentage(correctAnswers, deckLength) > 75
+              ? <Text style={styles.goodPercentage}>{questionPercentage(correctAnswers, deckLength)}%</Text>
+              : <Text style={styles.badPercentage}>{questionPercentage(correctAnswers, deckLength)}%</Text>
+            }
+
           </View>
-          : <View>
-            <Text>{questionList[questionsAnswered].question}?</Text>
+          : <View style={styles.results}>
+            <Text style={styles.question}>{questionList[questionsAnswered].question}?</Text>
             <TouchableOpacity
               onPress={this.toggleAnswer}>
               {seeAnswerToggle
-                ? <Text>
+                ? <Text style={styles.answer}>
                   {questionList[questionsAnswered].answer}
                 </Text>
-                : <Text>
-                  See Answer
+                : <Text style={styles.answer}>
+                  Click To See Answer
                   </Text>}
             </TouchableOpacity>
+            <CorrectBtn onPress={this.submitCorrect} />
+            <IncorrectBtn onPress={this.submitIncorrect} />
           </View>}
 
 
@@ -46,6 +96,98 @@ class Quiz extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  question: {
+    fontSize: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  answer: {
+    fontSize: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  iosCorrectBtn: {
+    backgroundColor: lightGreen,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  androidCorrectdBtn: {
+    backgroundColor: lightGreen,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  correctBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  iosIncorrectBtn: {
+    backgroundColor: tomatoRed,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  androidIncorrectBtn: {
+    backgroundColor: tomatoRed,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  incorrectBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  badPercentage: {
+    color: tomatoRed,
+    fontSize: 120,
+    textAlign: 'center',
+  },
+  goodPercentage: {
+    color: lightGreen,
+    fontSize: 120,
+    textAlign: 'center',
+  },
+  correctQuestions: {
+    justifyContent: 'flex-start',
+    fontSize: 40,
+    paddingBottom: 60,
+    textAlign: 'center'
+  },
+  results: {
+    justifyContent: 'flex-start'
+  }
+})
 function mapStateToProps(state, { route }) {
   const { id } = route.params;
   const questionList = state[id].cards;
