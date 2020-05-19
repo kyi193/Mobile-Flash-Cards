5be33f4d34b9f1d46a59f45a3f1952bdcf1febc2
Kevin <kevin.yi93@gmail.com>
2020-05-18 20:05:45 -0400

    Add textbox to type in topic of of the deck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
 import { addDeck } from '../actions'
 import { saveDeck } from '../utils/api'
 
@@ -10,12 +10,25 @@ class AddDeck extends Component {
     this.state = {
       input: '',
     }
+    this.onChangeText = this.onChangeText.bind(this)
+  }
+
+  onChangeText(text) {
+    this.setState(() => ({
+      input: text
+    }))
   }
 
   render() {
+    const { input } = this.state
     return (
       <View>
         <Text>What is the topic of this deck?</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => this.onChangeText(text)}
+          value={input}
+        />
       </View>
     )
   }
